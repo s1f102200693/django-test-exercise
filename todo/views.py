d7771a8945a4b23bb6d1d947db18ef731607425c
1F10220024 <s1f102200244@iniad.org>
2023-07-20 11:26:18 +0900

    更新機能の編集

--- todo/views.py
+++ todo/views.py
@@ -43,9 +43,10 @@ def update(request, task_id):
         raise Http404 ("Task does not exist")
     if request.method == 'POST':
         task.title = request.POST ['title']
-        task.due_at = make_aware(parse_datetime(request.POST[ 'due_at' ]))
+        task.due_at = make_aware(parse_datetime(request.POST['due_at']))
         task.save()
-        return redirect (detail, task_id)
+        return redirect(detail, task_id)
+    
     context = {
         'task': task
     }
