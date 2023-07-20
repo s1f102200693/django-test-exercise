4296fe1d8d9c4450a1e9f415f57e86ce32cb277f
Miumaru-iniad <s1f102200637@iniad.org>
2023-07-20 11:53:24 +0900

    絞り込み指摘機能の追加

--- todo/views.py
+++ todo/views.py
@@ -5,18 +5,24 @@ from django.utils.dateparse import parse_datetime
 from todo.models import Task
 
 # Create your views here.
-
-
 def index(request):
     if request.method == 'POST':
         task = Task(title=request.POST['title'],
                     due_at=make_aware(parse_datetime(request.POST['due_at'])))
         task.save()
 
-    if request.GET.get('order') == 'due':
-        tasks = Task.objects.order_by('due_at')
+    status = request.GET.get('status', 'all')
+    if status == 'completed':
+        tasks = Task.objects.filter(completed=True)
+    elif status == 'not_completed':
+        tasks = Task.objects.filter(completed=False)
     else:
-        tasks = Task.objects.order_by('-posted_at')
+        tasks = Task.objects.all()
+
+    if request.GET.get('order') == 'due':
+        tasks = tasks.order_by('due_at')
+    else:
+        tasks = tasks.order_by('-posted_at')
 
     context = {
         'tasks': tasks,
@@ -30,7 +36,7 @@ def detail(request, task_id):
         task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
-    
+
     context = {
         'task': task,
         'now': now(),
@@ -52,7 +58,8 @@ def update(request, task_id):
     context = {
         'task': task
     }
-    return render(request, "todo/edit.htmI", context)
+    return render(request, "todo/edit.html", context)
+
 
 def close(request, task_id):
     try:
@@ -62,6 +69,7 @@ def close(request, task_id):
     task.completed = True
     task.save()
     return redirect(index)
+
 
 def delete(request, task_id):
     try:
